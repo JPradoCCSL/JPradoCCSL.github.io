60f08a8b3fcb111fefce91af3fb473c710522290
JPradoCCSL <09jordan.prado@institutmarina.cat>
2026-05-16 22:14:42 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+  getDatabase
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNN9RSVO01p7Xz9xzaBghSoNV-H3wQruI",
+  authDomain: "campuscrack-cbb41.firebaseapp.com",
+  databaseURL: "https://campuscrack-cbb41-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "campuscrack-cbb41",
+  storageBucket: "campuscrack-cbb41.firebasestorage.app",
+  messagingSenderId: "689398385446",
+  appId: "1:689398385446:web:be472bcebba66a6d67cfe7",
+  measurementId: "G-WBNBFMTW1M"
+};
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
